9fdbc970fda5498979b3e947ffc0aad7ed0c4057
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-12 18:41:14 +0200

    c

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -7,11 +7,12 @@
         // Tell Tableau we'd like to initialize our extension
         tableau.extensions.initializeAsync().then(function() {
 
-              // The first step in choosing a sheet will be asking Tableau what sheets are available
-              const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;      
+        // The first step in choosing a sheet will be asking Tableau what sheets are available
+        const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;      
 
         // Next, we loop through all of these worksheets and add buttons for each one
         worksheets.forEach(function(worksheet) {
+            
             // Declare our new button which contains the sheet name
             const button = $("<button></button>");
             button.text(worksheet.name);
