7e07cc94d91c1cc3a2557a9c4d8f001362684687
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-12 18:40:31 +0200

    c

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -6,24 +6,9 @@
     $(document).ready(function() {
         // Tell Tableau we'd like to initialize our extension
         tableau.extensions.initializeAsync().then(function() {
-            // Once the extension is initialized, ask the user to choose a sheet
-            showChooseSheetDialog();
-        });
-    });
 
-    /**
-     * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
-     */
-    function showChooseSheetDialog() {
-        // Clear out the existing list of sheets
-        $('#choose_sheet_buttons').empty();
-
-        // Set the dashboard's name in the title
-        const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
-        $('#choose_sheet_title').text(dashboardName);
-
-        // The first step in choosing a sheet will be asking Tableau what sheets are available
-        const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+              // The first step in choosing a sheet will be asking Tableau what sheets are available
+              const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;      
 
         // Next, we loop through all of these worksheets and add buttons for each one
         worksheets.forEach(function(worksheet) {
@@ -35,6 +20,7 @@
             $('#choose_sheet_buttons').append(button);
         });
 
-    }
+        });
+    });
 
 })();
