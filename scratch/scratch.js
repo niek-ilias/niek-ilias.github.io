51aa93470ae850253ad5ed898e640dd31163b680
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-12 19:07:07 +0200

    c

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -10,17 +10,17 @@
         tableau.extensions.initializeAsync().then(function() {
 
         // The first step in choosing a sheet will be asking Tableau what sheets are available
-        const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;      
+        const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;     
 
             // Next, we loop through all of these worksheets and add buttons for each one
             worksheets.forEach(function(worksheet) {
 
                 // Declare our new button which contains the sheet name
-                const button = $("<button></button>");
+                const button = $("<button type='button' class='btn btn-default btn-block'></button>");
                 button.text(worksheet.name);
 
                 // Add our button to the list of worksheets to choose from
-                $('#buttons').append(button);
+                $('#choose_sheet_buttons').append(button);
                 
             });
 
