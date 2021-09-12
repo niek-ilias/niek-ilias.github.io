88ae468c951fba5072a7c5be89e498f45e7c1125
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-12 19:01:54 +0200

    s

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -2,25 +2,27 @@
 
 // Wrap everything in an anonymous function to avoid polluting the global namespace
 (function() {
+
     // Use the jQuery document ready signal to know when everything has been initialized
     $(document).ready(function() {
+
         // Tell Tableau we'd like to initialize our extension
         tableau.extensions.initializeAsync().then(function() {
 
         // The first step in choosing a sheet will be asking Tableau what sheets are available
         const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;      
 
-        // Next, we loop through all of these worksheets and add buttons for each one
-        worksheets.forEach(function(worksheet) {
+            // Next, we loop through all of these worksheets and add buttons for each one
+            worksheets.forEach(function(worksheet) {
 
-            // Declare our new button which contains the sheet name
-            const button = $("<button></button>");
-            button.text(worksheet.name);
+                // Declare our new button which contains the sheet name
+                const button = $("<button></button>");
+                button.text(worksheet.name);
 
-            // Add our button to the list of worksheets to choose from
-            $('#buttons').append(button);
-            
-        });
+                // Add our button to the list of worksheets to choose from
+                $('#buttons').append(button);
+                
+            });
 
         });
     });
