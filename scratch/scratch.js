ba66ce98610a54640637cffe8bb30ec13993cbe8
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-13 11:05:51 +0200

    c

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -23,7 +23,7 @@
         worksheets.forEach(function(worksheet) {
             // Declare our new button which contains the sheet name
             const button = $("<button class='tooltip' slot='hotspot-bad' data-position='0.06789274904367737m 1.9420006412557558m -6.117739169877176m' data-normal='0.9997936950666919m 0.020276126701468552m -0.0012024936077523953m' data-visibility-attribute='visible'></button>");
-            button.text("<span class='tooltiptext'>"+worksheet.name+"</span>");
+            button.text('<span class="tooltiptext">'+worksheet.name+"</span>");
 
             // Add our button to the list of worksheets to choose from
             $('#model-viewer').append(button);
