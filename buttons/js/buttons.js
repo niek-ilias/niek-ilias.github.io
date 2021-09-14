b9548f3ba10ddced387a0bcc3a64eb1f3a7a4e99
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-14 14:47:06 +0200

    c

---
+++ buttons/js/buttons.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Wrap everything in an anonymous function to avoid polluting the global namespace
+(function() {
+    // Use the jQuery document ready signal to know when everything has been initialized
+    $(document).ready(function() {
+        // Tell Tableau we'd like to initialize our extension
+        tableau.extensions.initializeAsync().then(function() {
+            // Once the extension is initialized, ask the user to choose a sheet
+            createButtons();
+        });
+    });
+
+    /**
+     * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
+     */
+    function createButtons() {
+
+        // The first step in choosing a sheet will be asking Tableau what sheets are available
+        const worksheet = getSelectedSheet('Hotspot');
+
+        // Call to get the selected marks for our sheet
+        worksheet.getSummaryDataAsync().then(function(sumdata) {
+        // Get the first DataTable for our selected marks (usually there is just one)
+        const worksheetData = sumdata.data;
+
+        worksheetData.forEach(function(row) {
+
+            // Declare our new button which contains the sheet name
+            const button = $("<button class='workarea' kpi='"+row[4].value+"' slot='hotspot-"+row[0].value+"' data-position='"+row[1].value+"' data-normal='"+row[2].value+"' data-visibility-attribute='invisible'>"+row[0].value+"</button>");
+  
+            // Add our button to the list of worksheets to choose from
+            $('#model-viewer').append(button); 
+
+        });
+
+             });
+        
+    }
+
+
+    function getSelectedSheet(worksheetName) {
+        // Go through all the worksheets in the dashboard and find the one we want
+        return tableau.extensions.dashboardContent.dashboard.worksheets.find(function(sheet) {
+            
+            return sheet.name === worksheetName;
+        });
+    }
+
+})();
