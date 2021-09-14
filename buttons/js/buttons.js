5d4835f4e342debec7633220717f8a04fa10affd
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-14 15:38:13 +0200

    c

--- buttons/js/buttons.js
+++ buttons/js/buttons.js
@@ -2,15 +2,24 @@
 
 // Wrap everything in an anonymous function to avoid polluting the global namespace
 (function() {
+
     // Use the jQuery document ready signal to know when everything has been initialized
     $(document).ready(function() {
         // Tell Tableau we'd like to initialize our extension
         tableau.extensions.initializeAsync().then(function() {
             // Once the extension is initialized, ask the user to choose a sheet
             createButtons();
+
+            // Remove all buttons and recreate them based on new sheet
+            const worksheet = getSelectedSheet('Hotspot');
+            worksheet.addEventListener(tableau.TableauEventType.FilterChanged, function(event){
+                document.getElementById("model-viewer").innerHTML = "";
+                createButtons();
+            });
         });
     });
 
+    
     /**
      * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
      */
@@ -30,7 +39,7 @@
             const button = $("<button class='workarea' kpi='"+row[4].value+"' slot='hotspot-"+row[0].value+"' data-position='"+row[1].value+"' data-normal='"+row[2].value+"' data-visibility-attribute='invisible'><div class='tooltiptext'>"+row[0].value+"</div></button>");
   
             // Add our button to the list of worksheets to choose from
-            $('#model-viewer').append(button); 
+            $('#model-viewer').append(button);
 
         });
 
@@ -38,11 +47,9 @@
         
     }
 
-
     function getSelectedSheet(worksheetName) {
         // Go through all the worksheets in the dashboard and find the one we want
         return tableau.extensions.dashboardContent.dashboard.worksheets.find(function(sheet) {
-            
             return sheet.name === worksheetName;
         });
     }
