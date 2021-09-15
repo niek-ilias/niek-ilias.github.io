962cf2a51e56d65f389d74f0be78d02ec6b6e758
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-15 15:19:05 +0200

    c

--- buttons/js/buttons.js
+++ buttons/js/buttons.js
@@ -10,6 +10,8 @@
             // Once the extension is initialized, ask the user to choose a sheet
             createButtons();
 
+            initializeButtons();
+
             // Remove all buttons and recreate them based on new sheet
             const worksheet = getSelectedSheet('Hotspot');
             worksheet.addEventListener(tableau.TableauEventType.FilterChanged, function(event){
@@ -41,8 +43,21 @@
             // For each row in this table we wil now use the columns to populate the buttons
             worksheetData.forEach(function(row) {
 
-                // Declare our new button which contains the selected columns
-                const button = $("<button class='workarea' kpi='"+row[4].value+"' slot='hotspot-"+row[0].value+"' data-position='"+row[1].value+"' data-normal='"+row[2].value+"' data-visibility-attribute='invisible'>2<div class='tooltiptext'>"+row[3].value+"</div></button>");
+                // Declare our new button which contains the data for the selected row
+                // The columns in the sheet hotspot must be in the order below
+                // Column 0 is the Work Area
+                // Column 1 is the Data Position
+                // Column 2 is the Data Noraml
+                // Column 3 is the Tooltip Text
+                // Column 4 is the KPI Color
+                // Column 5 is the KPI Number
+                const button = $("<button class='workarea' kpi='"+row[4].value
+                                +"' slot='hotspot-"+row[0].value
+                                +"' data-position='"+row[1].value
+                                +"' data-normal='"+row[2].value
+                                +"' data-visibility-attribute='invisible'>"+row[5].value
+                                +"<div class='tooltiptext'>"+row[3].value
+                                +"</div></button>");
     
                 // Add our button to the model-viewer 
                 $('#model-viewer').append(button);
