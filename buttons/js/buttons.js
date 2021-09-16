a585fa6ffa666a55d69a2567d797d8d57cea8d55
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-16 17:54:50 +0200

    c

--- buttons/js/buttons.js
+++ buttons/js/buttons.js
@@ -66,8 +66,8 @@
     const clearButton = $("<button id='clearbutton'>Clear Filters</button>");
 
     function filterWorkarea(event) {
-        const worksheet = getSelectedSheet('Hotspot');
-        worksheet.selectMarksAsync('Work Area',[event.target.name],'replace');
+        const worksheet = getSelectedSheet('Work Orders');
+        worksheet.applyFilterAsync('Work Area',[event.target.name],'replace');
         // Add our button to the model-viewer 
         clearButton.click(clearWorkarea);
         $('#model-viewer').append(clearButton);
@@ -77,8 +77,8 @@
 
     function clearWorkarea(event)
     {
-        const worksheet = getSelectedSheet('Hotspot');
-        worksheet.selectMarksAsync('Work Area',[""],'all');
+        const worksheet = getSelectedSheet('Work Orders');
+        worksheet.applyFilterAsync('Work Area',[""],'all');
         $('#clearbutton').remove();
 
     }
