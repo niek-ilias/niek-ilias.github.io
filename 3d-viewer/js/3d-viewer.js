24a7879cdde024eec7abcf62a6f6306ff648dd8f
niek-ilias <niek.freriks@ilias-solutions.com>
2021-09-17 14:56:10 +0200

    c

--- 3d-viewer/js/3d-viewer.js
+++ 3d-viewer/js/3d-viewer.js
@@ -64,7 +64,7 @@
     }
 
     // This is the clear button that will be created after selecting a hotspot
-    const clearButton = $("<button class='reset' id='clearbutton'>Clear Filters</button>");
+    const clearButton = $('#model-viewer');
 
     // This function resets the filter set by filterWorkarea
     function filterWorkarea(event) {
@@ -73,8 +73,6 @@
         worksheet.applyFilterAsync('Work Area',[event.target.name],'replace');
         // Add our button to the model-viewer 
         clearButton.click(clearWorkarea);
-        $('#model-viewer').append(clearButton);
-       
 
     }
 
@@ -84,9 +82,6 @@
         const worksheet = getSelectedSheet('Work Orders');
         //All work areas will be selected
         worksheet.applyFilterAsync('Work Area',[""],'all');
-        //After clearing the filters the button will be removed
-        $('#clearbutton').remove();
-
         
     }
     
